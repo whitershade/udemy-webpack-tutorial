167914c9324599f828eb33a495e23cd74933b89c
whitershade <whitershade34@gmail.com>
2019-02-22 15:22:21 +0200

    added configuration for sass support

--- webpack.config.js
+++ webpack.config.js
@@ -22,6 +22,10 @@ module.exports = {
           'style-loader',
           'css-loader'
         ]
+      },
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader']
       }
     ]
   }
